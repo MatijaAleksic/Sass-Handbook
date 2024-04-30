3ac8b1b057e3eb5e184ac57a87ea172b8ac8f159
MatijaAleksic <34009136+MatijaAleksic@users.noreply.github.com>
2024-04-30 11:51:50 +0200

    Disabled purge for all css to be generated

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
  const {src, dest, watch, series } = require('gulp');
  const sass = require('gulp-sass')(require('sass'))
- const purgecss = require('gulp-purgecss');
+//  const purgecss = require('gulp-purgecss');
 
  //Funkcija koja ce compile Sass => Css i pipe ga u neki file
  function buildStyles(){
@@ -9,7 +9,7 @@
         //u rutu trazi sve html i da vidi koje klase i rule koristimo i izbaci sve rules koje ne koristimo
         //Ako izbacis index.html ili ga prebaci u novi folder da nije rutu i pokrenes gulp.js onda ce finalni css biti mnogo
         //samo ono sto se koristi u html-u ce biti ispisano
-        .pipe(purgecss({ content: ['*.html']})) 
+      //   .pipe(purgecss({ content: ['*.html']})) 
         .pipe(dest('css'))
  }
 
